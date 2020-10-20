40906fad3ae4de5b1fdcc882a2fd9544617e8612
Pablo996 <pascalstrahm96@gmail.com>
2020-10-20 13:37:06 +0200

    upload_5

--- hunde_konfigurator/just the program.py
+++ hunde_konfigurator/just the program.py
@@ -5,12 +5,6 @@ from json import loads, dumps
 import json
 
 
-"""
-Hier importieren wir die Files mit den Fragen. Die Files wurden als json geschrieben
-und können daher importiert und wie Dictionaries behandelt werden.
-Wenn die Files nicht richtig importiert werden können,
-wird durch das Try and Except eine Fehlermeldung ausgegeben.
-"""
 with open("fragen1.json", encoding="utf-8") as json_file:
     fragen = json.load(json_file)
 
@@ -18,24 +12,6 @@ nummer = 1
 
 frage = fragen[str(nummer)]["Frage"]
 antwort = fragen[str(nummer)]["Antwort"]
-
-
-app = Flask("Hunde Konfigurator")
-
-
-@app.route('/')
-@app.route('/home')
-def home():
-    return render_template('home.html')
-
-
-@app.route('/konfigurator')
-def konfigurator():
-    return render_template('konfigurator.html', frage=frage, antwort=antwort)
-
-
-if __name__ == "__main__":
-    app.run(debug=True, port=5006)
 
 
 def runde():
